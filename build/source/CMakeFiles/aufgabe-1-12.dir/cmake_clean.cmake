a79e30b00dbc0198cb35052e009dacfca2185b18
GottaGoGitHub <ericgrunitz@gmail.com>
2019-04-14 20:23:23 +0200

    commit for all executables 20:23

---
+++ build/source/CMakeFiles/aufgabe-1-12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe-1-12.dir/aufgabe-1-12.cpp.o"
+  "aufgabe-1-12.pdb"
+  "aufgabe-1-12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe-1-12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/source/CMakeFiles/fract_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fract_2.dir/fract_2.cpp.o"
+  "fract_2.pdb"
+  "fract_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fract_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
